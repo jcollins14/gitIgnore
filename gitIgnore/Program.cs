c2529cdf772c707c7c3794c9848cca82fc10c3df
Jake Collins <jakecollins1014@gmail.com>
2020-02-06 18:46:35 -0500

    init commit

---
+++ gitIgnore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
